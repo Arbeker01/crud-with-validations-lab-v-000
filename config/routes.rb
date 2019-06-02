eae137253b4f50eb3e3b8ce54ea816344c4dfe13
Romeo Gono <romeogono1@aol.com>
2019-06-02 13:21:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
     patch '/songs/:id', to: 'songs#update'
     delete '/songs/:id', to: 'songs#destroy'
-  
+
     root 'songs#index'
 end
